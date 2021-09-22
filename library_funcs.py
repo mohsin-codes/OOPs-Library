984c47fef0d8a630242118a77ad1fd5ae837582f
Mohsin <mohsink9899@gmail.com>
2021-09-22 19:38:41 +0530

    First Commit

---
+++ library_funcs.py
@@ -0,0 +1,21 @@
+class Library:
+    def __init__(self, book_list, lib_name):
+        self.book_list = book_list
+        self.name = name
+        self.lendDict = {}
+
+
+    def display_book(self):
+        pass
+
+    def lend_book(self, book, user):
+        pass
+
+    def add_book(self, book):
+        pass
+
+    def return_book(self, user):
+        pass
+
+
+gg
